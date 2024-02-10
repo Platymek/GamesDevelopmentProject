bbc47da94b7c9bccefcc2747158cec5b20f8abd0
Platymek <hectordavid.selby@gmail.com>
2024-02-10 17:31:11 +0000

    Player: rotate tank in direction of turret aim

--- Scenes/Actors/Player/Player.cs
+++ Scenes/Actors/Player/Player.cs
@@ -6,9 +6,6 @@ using System;
 // add properties to attack stats file:
 // - add cancellable boolean
 
-// add ability to move:
-// - rotate tank in direction of turret aim
-
 // add ability to launch:
 // - add animation
 // - add launching physics
@@ -30,15 +27,27 @@ public partial class Player : CharacterBody3D
 {
 	// Properties //
 
-	[Export] private float _maxHorizontalSpeed = 4;
-	[Export] private float _maxFallingSpeed = 4;
-	[Export] private float _fallAcceleration = 16;
+	[Export] private float _horizontalMaxSpeed = 4;
+	[Export] private float _horizontalAcceleration = 16;
+	[Export] private float _horizontalAirAcceleration = 4;
+	[Export] private float _horizontalDeceleration = 16;
+	[Export] private float _horizontalAirDeceleration = 4;
+	
+	[Export] private float _fallingMaxSpeed = 4;
+	[Export] private float _fallingAcceleration = 16;
+	[Export] private float _fallingDeceleration = 16;
+	
 	[Export] private float _rotationSpeed = 1;
-	[Export] private float _knockbackStrength = 2;
+	[Export] private float _knockBackStrength = 2;
 	[Export] private int _maxAmmoCount = 2;
 	[Export] private float _launchHeight = 4;
 
 	private Node3D _turret;
+
+	private bool _accelerating = false;
+	
+	
+	// Node Functions //
 	
 	// get movement vector rotated relative to camera
 	private Vector2 MoveVector
@@ -49,11 +58,8 @@ public partial class Player : CharacterBody3D
 				"Down", "Up",
 				"Right", "Left");
 
-			GD.Print(GetViewport().GetCamera3D().GlobalRotation.Y);
-
 			Vector2 rotatedMove = move.Rotated(
-				GetViewport().GetCamera3D().GlobalRotation.Y
-				- Rotation.Y);
+				GetViewport().GetCamera3D().GlobalRotation.Y);
 
 			return rotatedMove;
 		}
@@ -68,14 +74,24 @@ public partial class Player : CharacterBody3D
 		base._Ready();
 
 		_turret = GetNode<Node3D>("Model/TankBase/TankTurret");
+
+		_accelerating = false;
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	// Called every frame. 'delta' is the elapsed time
+	// since the previous frame.
 	public override void _Process(double delta)
 	{
 		base._Process(delta);
+		
+		// move based on velocity from previous frame
+		MoveAndSlide();
+		
+		
+		// Horizontal Movement //
 
 		Vector2 move = MoveVector;
+		_accelerating = false;
 		
 		// if the stick is being moved
 		if (move != Vector2.Zero)
@@ -85,20 +101,124 @@ public partial class Player : CharacterBody3D
 			
 			// rotate turret in direction of stick
 			_turret.Rotation = new Vector3(
-				0, turretRotation, 0);
+				0, turretRotation - Rotation.Y, 0);
+
+			Accelerate(turretRotation, delta);
+		}
+	
+		// get horizontal velocity
+		var currentVelocity = new Vector2(Velocity.X, Velocity.Z);
+		
+		// get current speed
+		float currentSpeed = currentVelocity.Length();
+
+		// if going faster than max speed or not accelerating, decelerate
+		if (currentSpeed > _horizontalMaxSpeed || !_accelerating)
+		{
+			// decelerate
+			float newSpeed = currentSpeed 
+							 - _horizontalDeceleration
+							 * (float)delta * (IsOnFloor() 
+								 ? _horizontalDeceleration
+								 : _horizontalAirDeceleration);
 			
-			// move forwards regardless of stick angle
-			Velocity += Vector3.Forward.Rotated(
-				Vector3.Up, Rotation.Y);
+			// if deceleration makes the value below max speed and the player
+			// is accelerating, cap to max speed
+			if (newSpeed < _horizontalMaxSpeed && _accelerating)
+			{
+				newSpeed = _horizontalMaxSpeed;
+			}
 			
-			// move stick in direction of 
+			// if deceleration reduced the new speed below 0, set to 0
+			if (newSpeed < 0)
+			{
+				newSpeed = 0;
+			}
+				
+			// normalise velocity and multiply by the new speed to
+			// retain angle
+			var newVelocity = currentVelocity.Normalized() 
+								  * newSpeed;
+
+			Velocity = new Vector3(newVelocity.X, Velocity.Y, 
+				newVelocity.Y);
+		}
+		
+		
+		// Vertical Movement //
+
+		if (!IsOnFloor())
+		{
+			float fallingSpeed = -Velocity.Y;
+
+			fallingSpeed += _fallingAcceleration * (float)delta;
+
+			if (fallingSpeed > _fallingMaxSpeed)
+			{
+				fallingSpeed = _fallingMaxSpeed;
+			}
+			
+			Velocity = new Vector3(Velocity.X, -fallingSpeed, Velocity.Z);
 		}
 
-		MoveAndSlide();
+		if (IsOnFloorOnly())
+		{
+			Velocity = new Vector3(Velocity.X, 0, Velocity.Z);
+		}
 	}
 	
 	
 	// Other Functions //
 
-	
+	private void Accelerate(float angle, double delta)
+	{
+		// accelerate forwards regardless of stick angle
+		Velocity += Vector3.Forward.Rotated(Vector3.Up, Rotation.Y)
+					* (float)delta * (IsOnFloor() 
+						? _horizontalAcceleration
+						: _horizontalAirAcceleration);
+
+		_accelerating = true;
+
+		TurnTowards(angle, delta);
+	}
+
+	private void TurnTowards(float targetAngle, double delta)
+	{
+		float currentAngle = Rotation.Y;
+		
+		// get angle difference from current angle to new angle
+		float angleDifference = Mathf.AngleDifference(
+			currentAngle, targetAngle);
+		
+		// if the difference is 0, no rotation is needed
+		if (angleDifference == 0) return;
+
+		float rotationSpeed = Mathf.Tau * _rotationSpeed * (float)delta;
+		
+		// if the difference in angle is smaller than the rotation speed
+		// snap to target angle and return
+		if (MathF.Abs(angleDifference) < rotationSpeed)
+		{
+			// apply rotation
+			Rotation = new Vector3(
+				Rotation.X,
+				targetAngle,
+				Rotation.Z);
+
+			return;
+		}
+
+		// check direction of turning and turn in tha direction
+		float newAngle = currentAngle 
+						   + (angleDifference < 0
+							   ? -rotationSpeed
+							   : rotationSpeed);
+		
+		// apply rotation
+		Rotation = new Vector3(
+			Rotation.X,
+			newAngle,
+			Rotation.Z);
+	}
 }
